08a69209344abdc0f4c65917714763c73dc833a5
richard leandro <negrelirichard@gmail.com>
2026-06-13 16:56:58 -0300

    Inplementei uma limitação para o cliente conseguir marcar horario de dois meses

--- Barbearia/firebaseConfig.js
+++ Barbearia/firebaseConfig.js
@@ -2,7 +2,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyCIkN4dtsMy_XCoGucHWrzOGeDtDbmD9SY",
+  authDomain: "barbearia-tcc123.firebaseapp.com",
+  projectId: "barbearia-tcc123"
 };
 
 const app = initializeApp(firebaseConfig);
